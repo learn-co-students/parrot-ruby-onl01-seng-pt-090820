aa60e3cf990c704d96a68a1e0750b301cc4c7e88
S1umish <sunitee.mishra@gmail.com>
2020-09-11 17:43:45 -0400

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,13 +1,6 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
-def parrot
-    puts "Squawk!"  # output when called without any arguments
-    return "Squawk!" #return ,"Squawk!" #when called without any arguement
-    puts "#{phrase}" #return the default phrase, "Squawk!"
-end
 def parrot(phrase = "Squawk!")
-    puts "#{phrase}" #return the default phrase, "Squawk!"
+    puts phrase
+    return phrase
 end
-
-# "Squawk!" == "Pretty bird!"
-# puts "#{phrase}"
